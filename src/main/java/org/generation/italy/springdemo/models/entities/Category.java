b41c2ce49cf548945830acbe8e22734e022a2ab7
Zomerzz <aresufilippo2005@gmail.com>
2025-05-05 13:07:47 +0200

    Aggiunge Category e supplier noltre debugga

---
+++ src/main/java/org/generation/italy/springdemo/models/entities/Category.java
@@ -0,0 +1,48 @@
+package org.generation.italy.springdemo.models.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoryid")
+    private int categoryId;
+    @Column (name = "categoryname")
+    private String categoryName;
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public Category() {
+    }
+
+    public Category(String categoryName, String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
