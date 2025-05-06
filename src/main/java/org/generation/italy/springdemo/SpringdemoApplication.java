e9028f175bb4a11fe8680a03300a99ae4c3e2600
N9C9 <nicolo.casertano@gmail.com>
2025-05-06 17:56:05 +0200

    implementa esempi di utilizzo di query JPQL con Spring Data JPA

--- src/main/java/org/generation/italy/springdemo/SpringdemoApplication.java
+++ src/main/java/org/generation/italy/springdemo/SpringdemoApplication.java
@@ -1,15 +1,9 @@
 package org.generation.italy.springdemo;
 
 import jakarta.persistence.EntityManager;
-import org.generation.italy.springdemo.models.entities.Product;
-import org.generation.italy.springdemo.models.exceptions.DataException;
-import org.generation.italy.springdemo.models.repositories.CustomProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 public class SpringdemoApplication {
