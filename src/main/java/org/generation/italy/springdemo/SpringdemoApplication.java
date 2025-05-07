a791ffd9bd4b722eb4ef9b055f0c992143dc460f
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-05-08 00:20:39 +0200

    Rimuove cose aggiunte al main per colpa di un merge

--- src/main/java/org/generation/italy/springdemo/SpringdemoApplication.java
+++ src/main/java/org/generation/italy/springdemo/SpringdemoApplication.java
@@ -1,7 +1,5 @@
 package org.generation.italy.springdemo;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
